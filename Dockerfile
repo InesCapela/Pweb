ea7ee8c33155e5badaab6aaa287ee93bac580e70
Ines <ines.capela@infolabix.com>
2020-10-18 21:55:58 +0100

    check ping pong

--- Dockerfile
+++ Dockerfile
@@ -9,3 +9,4 @@ LABEL maintainer="Ines <36502@ufp.edu.pt>" \
       version="1.0"
 
 CMD go run public/main.go
+
--- public/main.go
+++ public/main.go
@@ -3,13 +3,13 @@ package main
 import "fmt"
 import "github.com/gin-gonic/gin"
 
-
 func main() {
 	fmt.Println("hello world")
 
 	f := fib()
 	fmt.Println(f(),f(),f(),f(),f())
 
+	//r router H header
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
@@ -19,10 +19,5 @@ func main() {
 	r.Run()
 }
 
-func fib() func() int{
-	a, b :=0,1
-	return func() int {
-		a, b = b, a+b
-		return a
-	}
-}
+
+//calculadora, várias operações para cada operação
