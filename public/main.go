a06cf047c4f93a4d3501c3ab510736871a47182a
Ines <ines.capela@infolabix.com>
2020-10-19 17:06:08 +0100

    organizate classes

--- public/main.go
+++ public/main.go
@@ -1,23 +1,16 @@
 package main
 
 import "fmt"
-import "github.com/gin-gonic/gin"
 
 func main() {
+	// HELLO WORLD
 	fmt.Println("hello world")
 
+	//FIB
 	f := fib()
 	fmt.Println(f(),f(),f(),f(),f())
 
-	//r router H header
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run()
+	ping()
 }
-
 
 //calculadora, várias operações para cada operação
